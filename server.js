47f0f0dfa1622780acab58f4774486d9fb6c26a4
Bhavesh Kapoor <bhaveshkapoor@Bhaveshs-MacBook-Air.local>
2026-06-13 09:34:24 +0530

    initial-setup

---
+++ server.js
@@ -0,0 +1,5 @@
+import app from './src/app.js'
+
+app.listen(4000,(req,res)=>{
+    console.log("server is running at this port")
+})
---
+++ src/utills/config.js
@@ -0,0 +1,6 @@
+import env from 'dotenv'
+env.config()
+
+APP_CONFIG={
+    // SERVER_PORT :  process.b
+}
